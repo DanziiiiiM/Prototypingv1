79dc3d25d477daff63cb2b3096620ddc67f4212d
DanziiiiiMgit config --global user.email danjen04@iba.dkgit config --global user.name DanziiiiiM <danjen04@iba.dk>
2025-09-29 12:54:31 +0200

    29-09-2025

--- script.js
+++ script.js
@@ -22,4 +22,33 @@ const booknu = document.getElementById("book-nu");
 booknu.addEventListener("click", function () {
   alert('BOOKET');
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function opSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function nuSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mineSlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
   
